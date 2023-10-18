2e077fb5f9b5e4f8875ca14cbceb26c6d5a82749
Olzhas Askar <olzhas.askar@gmail.com>
2023-10-18 08:50:14 +0200

    feat: look up identifiers

--- token/token.go
+++ token/token.go
@@ -33,3 +33,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET = "LET"
 )
+
+var keywords = map[string]TokenType{
+    "let": LET,
+    "fn": FUNCTION,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+	return tok
+    }
+    return IDENT
+}
