26b4ad09769861e0f14de5e5f482aa89098dc6e1
Olzhas Askar <olzhas.askar@gmail.com>
2023-12-20 22:59:01 +0100

    feat: prefix expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,6 +7,25 @@ import (
 	"testing"
 )
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct{
+		input string
+		expected bool
+	} {
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!5", true},
+		{"!!true", true},
+		{"!!false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func TestEvalBooleanExpression(t *testing.T) {
 	tests := []struct{
 		input string
@@ -43,6 +62,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	} {
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
