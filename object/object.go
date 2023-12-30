75f0534c7e2bbe6290840909dd5ef4da2c12ad33
Olzhas Askar <olzhas.askar@gmail.com>
2023-12-30 23:46:18 +0100

    feat: function object

--- object/object.go
+++ object/object.go
@@ -1,7 +1,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"monkey/ast"
+	"strings"
 )
 
 type ObjectType string
@@ -10,6 +13,7 @@ const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ    = "NULL"
+	FUNCTION_OBJ    = "FUNCTION"
 	RETURN_VALUE_OBJ    = "RETURN_VALUE"
 	ERROR_OBJ    = "ERROR"
 )
@@ -34,6 +38,32 @@ func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 type Null struct {}
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string { return "null" }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+
+	return out.String()
+}
 
 type ReturnValue struct {
 	Value Object
